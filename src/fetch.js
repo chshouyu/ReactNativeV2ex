1f038330568316c9d3eb04aa548cfda16e5cca54
chshouyu <chshouyu@126.com>
2017-03-15 00:10:56 +0800

    use axios for http request

--- src/fetch.js
+++ src/fetch.js
@@ -1,8 +1,9 @@
+import axios from 'axios';
 import {
   REQUEST_LATEST_URL
 } from './constant';
 
 export async function fetchTopics() {
-  const res = await fetch(REQUEST_LATEST_URL);
-  return await res.json();
+  const res = await axios(REQUEST_LATEST_URL, { timeout: 1000 });
+  return res.data;
 }
