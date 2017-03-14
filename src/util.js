b1cd978b71971fb2b7b79d15340b98be94e09cd8
chshouyu <chshouyu@126.com>
2017-03-14 16:11:02 +0800

    add delay util function

--- src/util.js
+++ src/util.js
@@ -2,3 +2,11 @@
 export function redLog(message) {
   console.log('%c ' + message, 'color: #f00; font-weight: 700');
 }
+
+export function delay(time = 400) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
