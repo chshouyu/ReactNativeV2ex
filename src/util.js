f5648d8ba1cd259de5af5dff930cf51cde2a83a5
chshouyu <chshouyu@126.com>
2017-03-14 17:56:35 +0800

    add blueLog util function

--- src/util.js
+++ src/util.js
@@ -3,6 +3,10 @@ export function redLog(message) {
   console.log('%c ' + message, 'color: #f00; font-weight: 700');
 }
 
+export function blueLog(message) {
+  console.log('%c ' + message, 'color: blue; font-weight: 700');
+}
+
 export function delay(time = 400) {
   return new Promise(resolve => {
     setTimeout(() => {
